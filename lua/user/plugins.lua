6b384a5c207973062488027c185714f5970d1d1e
Lalith Tadigadapa <jordanator@users.noreply.github.com>
2022-01-19 08:26:30 +0530

    install shade

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -173,6 +173,8 @@ return packer.startup(function(use)
   use "rcarriga/nvim-dap-ui"
   use "Pocco81/DAPInstall.nvim"
 
+  use 'sunjon/shade.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
