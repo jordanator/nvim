61d038b27d2145cef6013603ca7e53453ad06a46
Lalith Tadigadapa <jordanator@users.noreply.github.com>
2022-08-27 21:38:39 +0530

    remove ts autocmd

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -181,9 +181,9 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 --   end,
 -- })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.ts" },
-  callback = function()
-    vim.lsp.buf.format { async = true }
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--   pattern = { "*.ts" },
+--   callback = function()
+--     vim.lsp.buf.format { async = true }
+--   end,
+-- })
